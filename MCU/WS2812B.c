828c4733ac9684c697232d2bfe35494ce532bab3
Sheep94 <mikolaj.owczarczak@student.put.poznan.pl>
2016-04-12 19:19:52 +0200

    Przeniesienie do podfolderu bibliotek do MCU

---
+++ MCU/WS2812B.c
@@ -0,0 +1,40 @@
+#include <avr/io.h>
+#include "WS2812B.h"
+#include <util/delay.h>
+void WS2812B_sendone(void)
+{
+	WS2812B_PORT|=(1<<WS2812B_PIN);
+	asm("nop");asm("nop");asm("nop");//t85
+	WS2812B_PORT&=~(1<<WS2812B_PIN);
+}
+void WS2812B_sendzero(void)
+{
+	WS2812B_PORT|=(1<<WS2812B_PIN);
+	asm("nop");asm("nop");//t85
+	WS2812B_PORT&=~(1<<WS2812B_PIN);
+}
+void WS2812B_send(uint8_t r, uint8_t g, uint8_t b)
+{
+		if(g&0x80)WS2812B_sendone();else WS2812B_sendzero();		if(g&0x40)WS2812B_sendone();else WS2812B_sendzero();		if(g&0x20)WS2812B_sendone();else WS2812B_sendzero();
+		if(g&0x10)WS2812B_sendone();else WS2812B_sendzero();		if(g&0x08)WS2812B_sendone();else WS2812B_sendzero();		if(g&0x04)WS2812B_sendone();else WS2812B_sendzero();
+		if(g&0x02)WS2812B_sendone();else WS2812B_sendzero();		if(g&0x01)WS2812B_sendone();else WS2812B_sendzero();
+	
+		if(r&0x80)WS2812B_sendone();else WS2812B_sendzero();		if(r&0x40)WS2812B_sendone();else WS2812B_sendzero();		if(r&0x20)WS2812B_sendone();else WS2812B_sendzero();
+		if(r&0x10)WS2812B_sendone();else WS2812B_sendzero();		if(r&0x08)WS2812B_sendone();else WS2812B_sendzero();		if(r&0x04)WS2812B_sendone();else WS2812B_sendzero();
+		if(r&0x02)WS2812B_sendone();else WS2812B_sendzero();		if(r&0x01)WS2812B_sendone();else WS2812B_sendzero();
+	
+		if(b&0x80)WS2812B_sendone();else WS2812B_sendzero();		if(b&0x40)WS2812B_sendone();else WS2812B_sendzero();		if(b&0x20)WS2812B_sendone();else WS2812B_sendzero();
+		if(b&0x10)WS2812B_sendone();else WS2812B_sendzero();		if(b&0x08)WS2812B_sendone();else WS2812B_sendzero();		if(b&0x04)WS2812B_sendone();else WS2812B_sendzero();
+		if(b&0x02)WS2812B_sendone();else WS2812B_sendzero();		if(b&0x01)WS2812B_sendone();else WS2812B_sendzero();
+}
+
+void WS2812_none(void)
+{
+	for(uint8_t i=0;i<nOfLEDs;i++)
+		WS2812B_send(0,0,0);
+}
+
+void WS2812B_init(void)
+{
+	WS2812B_DDR|=(1<<WS2812B_PIN);	
+}
